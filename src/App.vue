2a9cdf04ea82505eabd7db262bceb8710a4c0f1d
wusphinx <wu.sphinx@gmail.com>
2023-02-08 23:23:29 +0800

    feat: simple from 0

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from "vue";
+
+const user = ref("input name");
+const password = ref("input password");
+
+function login() {
+  // TODO: check password
+}
+</script>
+
+<template>
+  <h2>从0到1</h2>
+  <p><input v-model="user" /> {{ text }}</p>
+  <p><input v-model="password" /> {{ text }}</p>
+
+  <button @click="login">登录</button>
+</template>
